a5293bb2af72b357b3a4e2ef61a7c4b9c7400696
Yaowenqiang <yaowenqiang111@163.com>
2021-11-09 23:15:19 +0800

    add day12 uuid demo

---
+++ day12/src/main.rs
@@ -0,0 +1,11 @@
+use uuid::Uuid;
+
+fn main() -> Result<(), uuid::Error>{
+
+    let my_uuid = Uuid::new_v4();
+    println!("{}", my_uuid);
+
+    let my_uuid2 = Uuid::parse_str("63da2896-8e16-4944-aae4-0bddf471fb4f")?;
+    println!("{}", my_uuid2.to_urn());
+    Ok(())
+}

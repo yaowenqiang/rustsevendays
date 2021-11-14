d0b2fefc746f64a84a78c5270d54c5ee2c9b36a7
yaowenqiang <yaowenqiang111@163.com>
2021-11-14 23:54:23 +0800

    fix typo

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,8 +1,8 @@
 extern crate redis;
-use redis::{Client, Commands, connection, RedisResult};
+use redis::{Client, Commands, Connection, RedisResult};
 
 fn main() {
     println!("24 days of Rust - redis (day 18)");
-    let client = client::open("redis://127.0.0.1/").unwrap();
+    let client = Client::open("redis://127.0.0.1/").unwrap();
     let conn = client.get_connection().unwrap();
 }

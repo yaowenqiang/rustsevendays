175506593deb5cd2fca80b98b6038e24038f32fb
yaowenqiang <yaowenqiang111@163.com>
2021-11-15 23:41:17 +0800

    add redis run sadd commands demo

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,8 +1,22 @@
 extern crate redis;
 use redis::{Client, Commands, Connection, RedisResult};
+use std::collections::HashSet;
+type UserId = u64;
 
 fn main() {
     println!("24 days of Rust - redis (day 18)");
-    let client = Client::open("redis://127.0.0.1/").unwrap();
-    let conn = client.get_connection().unwrap();
+    let mut client = Client::open("redis://127.0.0.1/").unwrap();
+    let mut conn = client.get_connection().unwrap();
+    let my_id: UserId = 1;
+    let their_id: UserId = 2;
+
+    add_friend(&mut conn,my_id, their_id).expect("friendship failed!");
+}
+
+fn add_friend(conn: &mut Connection, my_id: UserId, their_id: UserId) -> RedisResult<()> {
+    let my_key = format!("friends:{}", my_id);
+    let their_key = format!("friends:{}", their_id);
+    conn.sadd(my_key, their_id)?;
+    conn.sadd(their_key, my_id)?;
+    Ok(())
 }

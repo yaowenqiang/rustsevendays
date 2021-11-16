04c5b11f645082038105082a74c1e43d16b55f68
yaowenqiang <yaowenqiang111@163.com>
2021-11-16 23:47:17 +0800

    add sinter redis demo

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -11,6 +11,17 @@ fn main() {
     let their_id: UserId = 2;
 
     add_friend(&mut conn,my_id, their_id).expect("friendship failed!");
+
+    let answer: i32 = conn.get("answer").unwrap();
+    println!("Answer: {}", answer);
+
+    for i in 1 ..10u64 {
+        add_friend(&mut conn, i, i + 2).expect("Friendship failed :(");
+    }
+
+    println!("You have {} friends in common.", 
+             friends_in_common(&mut conn, 2, 3).
+             map(|s| s.len()).unwrap_or(0));
 }
 
 fn add_friend(conn: &mut Connection, my_id: UserId, their_id: UserId) -> RedisResult<()> {
@@ -19,4 +30,11 @@ fn add_friend(conn: &mut Connection, my_id: UserId, their_id: UserId) -> RedisRe
     conn.sadd(my_key, their_id)?;
     conn.sadd(their_key, my_id)?;
     Ok(())
+}
+
+fn friends_in_common(conn: &mut Connection, my_id: UserId, their_id: UserId) -> RedisResult<HashSet<UserId>> {
+    let my_key = format!("friends:{}", my_id);
+    let their_key = format!("friends:{}", their_id);
+    conn.sinter((my_key, their_key))
+
 }

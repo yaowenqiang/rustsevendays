38ea92b06cd83bfa5e43f0a5154fa55119ab3f24
yaowenqiang <yaowenqiang111@163.com>
2021-11-21 23:51:59 +0800

    add day 19 rayon demo

---
+++ day19/src/main.rs
@@ -0,0 +1,34 @@
+extern crate rayon;
+use rayon::prelude::*;
+
+fn semicircle(x: f64, r: f64) -> f64 {
+    (r * r - x * x).sqrt()
+}
+
+fn integrate<F>(f: F, points: usize) -> f64 
+where F: Fn(f64) -> f64 + Sync
+{
+    let delta = 1f64 / points as f64;
+    (0..points)
+        .map(|i| {
+            let a = i as f64 * delta;
+            delta *(f(a) + f(a + delta)) / 1f64
+        })
+        .sum()
+}
+fn parallel_integrate<F>(f: F, points: usize) -> f64 
+where F: Fn(f64) -> f64 + Sync
+{
+    let delta = 1f64 / points as f64;
+    (0..points)
+        .into_par_iter()
+        .map(|i| {
+            let a = i as f64 * delta;
+            delta *(f(a) + f(a + delta)) / 1f64
+        })
+        .sum()
+}
+fn main() {
+    println!("sequential: {}", 4f64 * integrate(semicircle, 10_000_000));
+    println!("parallel: {}", 4f64 * parallel_integrate(semicircle, 10_000_000));
+}

b3f453ef8b21811d0f54017245ef872946074a11
yaowenqiang <yaowenqiang111@163.com>
2021-11-24 23:19:31 +0800

    add dotenv usage demo

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,12 +1,20 @@
 extern  crate envy;
 #[macro_use]
 extern crate serde;
+extern crate dotenv;
 use serde::{Deserialize};
 
 #[derive(Deserialize, Debug)]
 struct Environment {
     lang: String,
 }
+
+#[derive(Deserialize, Debug)]
+struct Mailerconfig {
+    email_backend: String,
+    email_from: String,
+}
+
 
 use std::env;
 
@@ -19,6 +27,14 @@ fn main() {
     match envy::from_env::<Environment>() {
         Ok(environment) => println!("Language code: {}", environment.lang),
         Err(e) => println!("couldn't read LANG ({})", e),
-
     }
+
+    dotenv::dotenv().expect("Failed to read .env file");
+    println!("Email backend: {}", env::var("EMAIL_BACKEND").expect("EMAIL_BACKEND not found!"));
+
+    match envy::from_env::<Mailerconfig>() {
+        Ok(config) => println!("{:?}", config),
+        Err(e) => println!("Couldn't read mailer config ({})", e),
+    };
+
 }

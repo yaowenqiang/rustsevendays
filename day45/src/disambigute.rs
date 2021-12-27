cb1a70a2b74355a5447bda22047ee7aa77777797
Yaowenqiang <yaowenqiang111@163.com>
2021-12-27 23:41:14 +0800

    add disambiguating overlapping traits demo

---
+++ day45/src/disambigute.rs
@@ -0,0 +1,40 @@
+trait UsernameWidget {
+    fn get(&self) -> String;
+}
+
+trait AgeWidget {
+    fn get(&self) -> u8;
+}
+
+
+struct Form {
+    username: String,
+    age: u8,
+}
+
+
+impl UsernameWidget for Form {
+    fn get(&self) -> String {
+        self.username.clone()
+    }
+}
+impl AgeWidget for Form {
+    fn get(&self) -> u8 {
+        self.age
+    }
+}
+
+fn main() {
+    let form = Form{
+        username: "rustacean".to_owned(),
+        age: 20,
+    };
+
+    //println!("{}", form.get());
+
+    let username = <Form as UsernameWidget>::get(&form);
+    assert_eq!("rustacean".to_owned(), username);
+    let age = <Form as AgeWidget>::get(&form);
+    assert_eq!(20, age);
+    
+}

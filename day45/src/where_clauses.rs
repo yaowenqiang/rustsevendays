acbbbb601eddbfcfbf531b4a78af55014fc336e6
yaowenqiang <yaowenqiang111@163.com>
2022-01-05 23:35:52 +0800

    add where clauses demo

---
+++ day45/src/where_clauses.rs
@@ -0,0 +1,26 @@
+/*
+impl <A: Trait B + TraitC, D: TraitE + TraitF> MyTrait<A, D> for YourType {}
+
+impl <A, D> MyTrait<A,D> for YourType where
+    A: TraitB + TraitC,
+    D: TraitE, + TraitF {}
+
+    */
+
+use std::fmt::Debug;
+trait PrintInOption {
+    fn print_in_option(self);
+}
+
+
+impl <T> PrintInOption for T where
+Option<T>: Debug {
+    fn print_in_option(self) {
+        println!("{:?}", Some(self));
+    }
+}
+
+fn main() {
+    let vec = vec![1,2,3];
+    vec.print_in_option();
+}

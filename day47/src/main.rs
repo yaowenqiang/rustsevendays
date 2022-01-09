e88ae792bed0454ec437fea7dea43d4ebbb29d60
yaowenqiang <yaowenqiang111@163.com>
2022-01-09 23:40:12 +0800

    add scoping rules demo

---
+++ day47/src/main.rs
@@ -0,0 +1,28 @@
+struct ToDrop;
+
+impl Drop for ToDrop {
+    fn drop(&mut self) {
+        println!("ToDrop is being dropped");
+    }
+}
+fn create_box() {
+    let _box1 = Box::new(3i32);
+}
+
+fn main() {
+    let _box2 = Box::new(5i32);
+
+    {
+        let _box3 = Box::new(4i32);
+    }
+
+    for _ in 0u32..1_000 {
+        create_box();
+    }
+
+    let x = ToDrop;
+    println!("Made a ToDrop");
+
+
+}
+//rustc main.rs &7 valgrind ./main

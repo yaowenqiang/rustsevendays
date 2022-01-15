cab6aff480402011fd423b3c3fad53ffd5d17d9d
Yaowenqiang <yaowenqiang111@163.com>
2022-01-15 23:26:20 +0800

    add traits lifetime demo

---
+++ day47/src/traits.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+struct Borrowed<'a> {
+    x: &'a i32
+}
+
+impl<'a> Default for Borrowed<'a> {
+    fn default() -> Self {
+        Self {
+            x: &10,
+        }
+    }
+}
+
+fn main() {
+    let b: Borrowed = Default::default();
+    println!("b is {:?}", b);
+}

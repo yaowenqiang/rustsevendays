21764d817e580b211cd077324220235396515fb5
Yaowenqiang <yaowenqiang111@163.com>
2022-01-19 22:34:21 +0800

    add more lifetime  examples

--- day47/src/lifetime.rs
+++ day47/src/lifetime.rs
@@ -9,4 +9,34 @@ fn main() {
         let borrow2 = &i;
         println!("borrow2: {}", borrow2);
     }
+
+    {
+        let x = 0;
+        {
+            //let y = 42;
+            let y = 42;
+            //x = &y;
+        }
+        println!("the value of 'x' is {}", x);
+    }
+
+    let string1 : String = String::from("abcd");
+    {
+        let string2 = "xyz";
+        let result;
+        {
+            result = longest(string1.as_str(), string2);
+            {
+                println!("The longest string is {}", result);
+            }
+        }
+    }
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
